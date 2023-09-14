8468ac79a2461a9e7d9e639dc79c8db56b7e070a
Jaweria B <bjaweria509@gmail.com>
2023-09-14 21:08:56 +0500

    update our menu-items to navigate using withNavigate

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import './App.css';
 import HomePage from './pages/HomePage/HomePage.component';
+import { Routes, Route } from 'react-router-dom';
 
+const HatsPage = () => (
+  <div>
+    <h1>Hats Page</h1>
+  </div>
+)
 function App() {
   return (
     <div>
-      <HomePage/>
+      <Routes>
+        <Route exact path='/' Component={HomePage} />
+        <Route index path='/shop/hats' Component={HatsPage} />
+      </Routes>
     </div>
   );
 }
