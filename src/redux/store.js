aadac13ff1c1077f856c2ba0bdc131a0a255b3fa
Jaweria B <bjaweria509@gmail.com>
2023-10-20 13:41:07 +0500

    added persistor in our app using react-persist

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,32 +1,40 @@
 import {compose, legacy_createStore as createStore, applyMiddleware} from "redux";
-// import logger from "redux-logger";
+import logger from "redux-logger";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import createSagaMiddlewares from 'redux-saga';
 
 import { rootReducer } from "./root-reducer";
 
 
-const loggerMiddleware = (store) => (next) => (action) => {
-    if (!action.type) {
-      return next(action);
-    }
-  
-    console.log('type: ', action.type);
-    console.log('payload: ', action.payload);
-    console.log('currentState: ', store.getState());
-  
-    next(action);
-  
-    console.log('next state: ', store.getState());
-};
-  
+const sagaMiddlewares = createSagaMiddlewares();
 
-// const middleWares = [process.env.NODE_ENV === 'development' && logger].filter(
-//     Boolean
-// );
+const middleWares = [process.env.NODE_ENV === 'development' && logger].filter(
+    Boolean
+);
 
-const middleWares = [loggerMiddleware];
+const composeEnhancer = 
+  (process.env.NODE_ENV != 'production' &&
+    window &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ||
+    compose);
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  blacklist: 'user'
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 const composedEnhancers = compose(applyMiddleware(...middleWares));
 
-export const store = createStore(rootReducer, undefined, composedEnhancers);
+export const store = createStore(
+  persistedReducer,
+  undefined,
+  composedEnhancers);
+
+export const persistor = persistStore(store);
 
 
 
@@ -35,7 +43,21 @@ export const store = createStore(rootReducer, undefined, composedEnhancers);
 
 
 
+// const loggerMiddleware = (store) => (next) => (action) => {
+//     if (!action.type) {
+//       return next(action);
+//     }
+  
+//     console.log('type: ', action.type);
+//     console.log('payload: ', action.payload);
+//     console.log('currentState: ', store.getState());
+  
+//     next(action);
+  
+//     console.log('next state: ', store.getState());
+// };
 
+// const middleWares = [loggerMiddleware];
 
 
 
